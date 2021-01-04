cfd5a9ec49177f34eae07d14540fc936cd398049
Halleys-Comet <jhalley230@gmail.com>
2021-01-03 21:02:37 -0600

    add questions to javascript

---
+++ assets/js/script.js
@@ -0,0 +1,53 @@
+// create function to start quiz timer
+
+// create questions 
+
+const questions = [
+    {
+        question: 'Inside which HTML element do we put the JavaScrip?'
+        answers: [
+            {text: '<script>', correct: true},
+            {text: '<scripting>', correct: false},
+            {text: '<javascript>', correct: false},
+            {text: '<js>', correct: false}
+        ]
+    },
+
+    {
+        question: 'How do you create a function in JavaScript?'
+        answers: [
+            {text: 'function = myFunction()', correct: false},
+            {text: 'function myFunction()', correct: true},
+            {text: 'function:myFunction()', correct: false},
+            {text: 'funtion-myFunction()', correct: false}
+        ]
+    },
+
+    {
+        question: 'What is the correct way to write a JavaScript array?'
+        answers: [
+            {text:'var colors = ["red", "green", "blue"]', correct: true},
+            {text:'var colors = 1 = ("red"), 2 = ("green"), 3 = ("blue")', correct: false},
+            {text: 'var colors = "red", "green", "blue"', correct: false},
+            {text: 'var colors = (1:"red", 2:"green", 3:"blue")', correct: false}
+    ]
+},
+{
+    question: 'Which event occurs when the user clicks on an HTML element?'
+    answers: [
+        {text:'onclick', correct:true},
+        {text:'onchange', correct:false},
+        {text:'onmouseclick', correct:false},
+        {text:'onmouseover', correct:true}
+    ]
+}
+
+]
+
+
+
+
+
+
+
+// create function to subtract time on wrong answer
